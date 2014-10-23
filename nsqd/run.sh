8ec2d67c413cd4a1f07dbfca98dee34416fabf54
Ross Cooperman <rcooperman@movableink.com>
2014-10-23 14:42:43 -0400

    Advertise localhost as the broadcast address

--- nsqd/run.sh
+++ nsqd/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /gopath/src/github.com/bitly/nsq/apps/nsqlookupd/nsqlookupd &
-/gopath/src/github.com/bitly/nsq/apps/nsqd/nsqd --data-path=/data --lookupd-tcp-address=localhost:4160
+/gopath/src/github.com/bitly/nsq/apps/nsqd/nsqd --data-path=/data --lookupd-tcp-address=localhost:4160 --broadcast-address=localhost

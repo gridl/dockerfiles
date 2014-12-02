dffca11562e361f64be4a65522d4880e000b745b
Andrew Cohen <acohen@movableink.com>
2014-12-02 16:02:37 -0500

    Dont specify a patch level for ruby

--- ruby_2.1.4/rbenv.sh
+++ ruby_2.1.4/rbenv.sh
@@ -2,9 +2,9 @@
 export PATH="/home/ubuntu/.rbenv/bin:$PATH"
 
 # set default env vars
-export RBENV_VERSION=2.1.4-p0
+export RBENV_VERSION=2.1.4
 export RAILS_ENV=${RAILS_ENV:-test}
 
 # initialize and set ruby version
 eval "$(rbenv init -)"
-rbenv global 2.1.4-p0
+rbenv global 2.1.4
